2df2a686f5616d29480013af4d3234a4cf3d2927
UnibadanSU <unibadansu@gmail.com>
2022-03-18 00:49:47 +0100

    make changes

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn uisu_web_client.wsgi --log-file -
--- uisu_web_client/settings.py
+++ uisu_web_client/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+import cloudinary
+import cloudinary_storage
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y2%c1ca1nry6l=hx^&ol=&l9o-mkl5#!hwo)qmo)%2e*tb%&zz'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,10 +41,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'news',
+
+    'cloudinary',
+    'cloudinary_storage'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -80,6 +90,16 @@ DATABASES = {
     }
 }
 
+# DATABASE SETTINGS
+
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=600)            
+
+DATABASES['default'].update(db_from_env)
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -118,3 +138,29 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# CLOUDINARY SETTINGS
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'unibadansu',
+    'API_KEY': '676496872764576',
+    'API_SECRET' : '5iHtX-eB2xPdmYp03C5JtwhnoBw'
+}
+
+MEDIA_URL = "/media/"
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
+# WHITENOISE SETTINGS
+
+WHITENOISE_USE_FINDERS = True
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+BASE_DIR = Path(__file__).resolve().parent.parent
